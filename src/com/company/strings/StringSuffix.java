e0b590d1674de44877ea9470032206b8c29503e3
oscarnipps <oscarnips@gmail.com>
2020-08-24 23:43:58 +0100

    string suffixes retrieval

--- src/com/company/strings/StringSuffix.java
+++ src/com/company/strings/StringSuffix.java
@@ -1,7 +1,16 @@
 package com.company.strings;
 
-public class StringSuffix {
-    public static void getStringSuffixes(String value) {
+import java.util.ArrayList;
+import java.util.List;
 
+public class StringSuffix {
+    public static List<String> getStringSuffixes(String value) {
+        List<String> suffixList = new ArrayList<>();
+
+        for (int index = value.length() - 1; index >= 0; index--) {
+            suffixList.add(value.substring(index));
+        }
+
+        return suffixList;
     }
 }
--- src/com/company/strings/StringsImplementation.java
+++ src/com/company/strings/StringsImplementation.java
@@ -7,6 +7,15 @@ public class StringsImplementation {
         String value = "hello";
         System.out.println( "reversed string '" + value + "' --->  '" + StringReversal.reverseString("hello") +"'");
 
+        System.out.println();
+
+        //getting suffixes for a string
+        value = "house";
+        System.out.println("-----getting string suffixes for : '" + value + "'" );
+        StringSuffix.getStringSuffixes(value).forEach((suffixValue)-> System.out.println(suffixValue));
+
+        System.out.println();
+
 
     }
 }
